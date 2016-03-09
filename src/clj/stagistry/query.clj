f14b3a890ae24f5ccb323910658a2e5c6513020b
Brian Stearns <brianw.stearns@gmail.com>
2016-03-09 14:31:45 -0500

    Got create and select working to some extent

--- src/clj/stagistry/query.clj
+++ src/clj/stagistry/query.clj
@@ -10,4 +10,39 @@
 (use 'korma.db)
 (require '[clojure.string :as str])
 
-;; NEXT: create queries!
+;; ENTITIES
+
+(defentity art-pieces
+  (table :art_pieces)
+  (entity-fields
+    :id
+    :name
+    :description
+    :price
+    :artists_id)
+  (belongs-to artists))
+
+(defentity artists
+  (table :artists)
+  (entity-fields
+    :id
+    :name))
+
+;; QUERIES
+
+(defn all-pieces []
+  (select art-pieces
+          (fields
+            :id
+            :name
+            :description
+            :price)))
+
+(defn create-piece [piece]
+  (insert art-pieces
+          (values {
+                    :id (:id piece)
+                    :name (:name piece)
+                    :description (:description piece)
+                    :price (:price piece)})))
+
